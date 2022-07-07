e67a28a62443152ee192b09474c0478d5ce70750
lee.wang <67354376+oldwang12@users.noreply.github.com>
2022-07-07 11:32:56 +0800

    Update init.sh

--- deploy/init.sh
+++ deploy/init.sh
@@ -12,9 +12,7 @@ cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 name=Kubernetes
 baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+gpgcheck=0
 EOF
 
 # 安装指定版本
